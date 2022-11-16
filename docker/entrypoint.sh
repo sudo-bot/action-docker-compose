5cc74f4f82501e95200a313913d45bf3e1caa1a1
William Desportes <williamdes@wdes.fr>
2022-11-16 20:09:22 +0100

    Revert "Remove the custom entrypoint"
    This reverts commit 5e3eedc7cd8117bb1836adbb6eecd6fabc0391cc.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+##
+# @license http://unlicense.org/UNLICENSE The UNLICENSE
+# @author William Desportes <williamdes@wdes.fr>
+##
+
+set -e
+
+CLI_ARGS="$@"
+
+echo "::debug CLI_ARGS: ${CLI_ARGS}"
+
+docker-compose ${CLI_ARGS} 2>&1
